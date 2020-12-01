b0a71c4b515227e8edffe67f39b5a10d6467493f
Axel-Baudain <axel.baudain@gmail.com>
2020-12-01 09:02:01 +0100

    First commit

---
+++ precache-manifest.0091c645c8cefb47b688669cee036f35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879b3e477d36d1fd35fc0f677436aecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56fd8d682c2ce542fa6b",
+    "url": "/static/css/main.7bbc9f97.chunk.css"
+  },
+  {
+    "revision": "025d757440a43c1568a9",
+    "url": "/static/js/2.7b05dba2.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.7b05dba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fd8d682c2ce542fa6b",
+    "url": "/static/js/main.0861460f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "23e94c1a02bf59dce7946b35bde61f53",
+    "url": "/static/media/20201019_110817.23e94c1a.png"
+  },
+  {
+    "revision": "75b471c696ba3826c5d14343887c89de",
+    "url": "/static/media/arcade.75b471c6.jpg"
+  },
+  {
+    "revision": "ac4abf49f6473e9bf0c6ca01474d2ae3",
+    "url": "/static/media/library.ac4abf49.jpg"
+  },
+  {
+    "revision": "1623430169f741e3b40d674911be9eb4",
+    "url": "/static/media/logo.16234301.svg"
+  },
+  {
+    "revision": "1e968268979c506409161f72321fd101",
+    "url": "/static/media/pocketwatch.1e968268.jpg"
+  },
+  {
+    "revision": "b73e93db12f3388d3ece4df7362875fa",
+    "url": "/static/media/rocket.b73e93db.jpg"
+  }
+]);
